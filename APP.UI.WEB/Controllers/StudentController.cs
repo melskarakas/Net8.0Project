ff3dd6134f3e86d74f8a5e1007ad6f9e5c6a7a10
Melis Karakas <melskarakas@gmail.com>
2026-03-13 22:07:11 +0300

    Student Controller eklendi

---
+++ APP.UI.WEB/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APP.UI.WEB.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetStudents()
+        {
+            return View();
+        }
+    }
+}
